b1aacd3bb5bafc832ca98a7e5f06fc2c129e1294
Gabriel Stamato <gabrielstamato.emix@hotmail.com>
2020-01-07 13:20:48 -0300

    Add StorageController

--- app/Controllers/Http/StorageController.js
+++ app/Controllers/Http/StorageController.js
@@ -1,92 +1,52 @@
 'use strict'
 
-/** @typedef {import('@adonisjs/framework/src/Request')} Request */
-/** @typedef {import('@adonisjs/framework/src/Response')} Response */
-/** @typedef {import('@adonisjs/framework/src/View')} View */
-
-/**
- * Resourceful controller for interacting with storages
- */
+const Storage = use('App/Models/Storage')
 class StorageController {
-  /**
-   * Show a list of all storages.
-   * GET storages
-   *
-   * @param {object} ctx
-   * @param {Request} ctx.request
-   * @param {Response} ctx.response
-   * @param {View} ctx.view
-   */
+
   async index ({ request, response, view }) {
+    const storages = await Storage.all()
+
+    return storages
   }
 
-  /**
-   * Render a form to be used for creating a new storage.
-   * GET storages/create
-   *
-   * @param {object} ctx
-   * @param {Request} ctx.request
-   * @param {Response} ctx.response
-   * @param {View} ctx.view
-   */
-  async create ({ request, response, view }) {
-  }
-
-  /**
-   * Create/save a new storage.
-   * POST storages
-   *
-   * @param {object} ctx
-   * @param {Request} ctx.request
-   * @param {Response} ctx.response
-   */
+  
   async store ({ request, response }) {
+    const data = request.all()
+
+    const storage = await Storage.create(data)
+
+    return storage
   }
 
-  /**
-   * Display a single storage.
-   * GET storages/:id
-   *
-   * @param {object} ctx
-   * @param {Request} ctx.request
-   * @param {Response} ctx.response
-   * @param {View} ctx.view
-   */
   async show ({ params, request, response, view }) {
+    const storage = await Storage.findOrFail(params.id)
+
+    return storage
   }
 
-  /**
-   * Render a form to update an existing storage.
-   * GET storages/:id/edit
-   *
-   * @param {object} ctx
-   * @param {Request} ctx.request
-   * @param {Response} ctx.response
-   * @param {View} ctx.view
-   */
-  async edit ({ params, request, response, view }) {
-  }
 
-  /**
-   * Update storage details.
-   * PUT or PATCH storages/:id
-   *
-   * @param {object} ctx
-   * @param {Request} ctx.request
-   * @param {Response} ctx.response
-   */
   async update ({ params, request, response }) {
+    const storage = await Storage.findOrFail(params.id)
+
+    !name ? null : storage.name = name
+    !street ? null : storage.street = street
+    !number ? null : storage.number = number
+    !neighborhood ? null : storage.neighborhood = neighborhood
+    !city ? null : storage.city = city
+    !uf ? null : storage.uf = uf
+    !zipcode ? null : storage.zipcode = zipcode
+    !contact ? null : storage.contact = contact
+    !phone ? null : storage.phone = phone
+
+    storage.save()
+
+    return storage
   }
 
-  /**
-   * Delete a storage with id.
-   * DELETE storages/:id
-   *
-   * @param {object} ctx
-   * @param {Request} ctx.request
-   * @param {Response} ctx.response
-   */
   async destroy ({ params, request, response }) {
+    const storage = await Storage.findOrFail(params.id)
+
+    storage.delete()
   }
 }
 
