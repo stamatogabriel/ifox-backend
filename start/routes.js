def8c75e85b22d4de9ce1a41690f397aebe508a6
Gabriel Stamato <stamato7@gmail.com>
2020-01-25 10:59:33 -0300

    update routes

--- start/routes.js
+++ start/routes.js
@@ -3,39 +3,21 @@ const Route = use('Route')
 
 Route.post("/register", "AuthController.register");
 Route.post("/auth", "AuthController.authenticate");
-Route.get('/users', "AuthController.show")
+
 
 Route.group(() => {
+  Route.get('/users', "AuthController.show")
   Route.put("/logout", "AuthController.logout");
   Route.put("/change_password/:id", "AuthController.changePassword");
   Route.get('/get_user/:id', "AuthController.getUser");
   Route.delete('/delete_user/:id', 'AuthController.destroy')
-}).middleware("auth");
 
-Route.group(() => {
   Route.resource('drivers', "DriverController").apiOnly()
-}).middleware("auth");
-
-Route.group(() => {
   Route.resource('enterprises', 'EnterpriseController').apiOnly()
-}).middleware("auth");
-
-Route.group(() => {
   Route.resource('products', 'ProductController').apiOnly()
-}).middleware("auth");
-
-Route.group(() => {
   Route.resource('sellers', 'SellerController').apiOnly()
-}).middleware("auth");
-
-Route.group(() => {
   Route.resource('storages', 'StorageController').apiOnly()
-}).middleware("auth");
-
-Route.group(() => {
   Route.resource('vehicules', 'VehiculeController').apiOnly()
-}).middleware("auth");
-
-Route.group(() => {
   Route.resource('contracts', 'ContractController').apiOnly()
-})
+  Route.resource('partners', 'PartnersContractController').apiOnly()
+}).middleware("auth");
