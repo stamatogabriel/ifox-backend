b0f1570657ba0138fe985d21fb9704b58705ee4e
Gabriel Stamato <stamato7@gmail.com>
2020-03-07 21:34:37 -0300

    teste

--- app/Controllers/Http/SessionController.js
+++ app/Controllers/Http/SessionController.js
@@ -5,12 +5,12 @@ class SessionController {
   async store ({ request, response, auth }) {
     const { email, password } = request.all()
 
+    const token = await auth.attempt(email, password)
+
     const responseUser = await Database.table('users')
       .where('email', email)
 
     const user = responseUser[0]
-
-    const token = await auth.attempt(email, password)
 
     return { token, user }
   }
